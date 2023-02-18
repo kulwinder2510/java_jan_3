c23429f36b436d6291520e35624fb3e5df4da4b2
kulwinder2510 <kulwinder9053@gmail.com>
2023-02-18 16:07:59 -0500

    add numbers

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int number;
+//        number = 10;
+//        System.out.println("number is :" +number);
+
+//         int number1=10;
+//         int number2=34;
+//         int number3=43;
+//         int sum=number1+number2+number3;
+//        System.out.println("sum is :" +sum);
+
+//          int number1;
+//          int number2;
+//
+//          Scanner scanner = new Scanner(System.in);
+//          System.out.println("enter the value of number1 : ");
+//          number1=scanner.nextInt();
+//
+//        System.out.println("enter the value of number2 : ");
+//          number2=scanner.nextInt();
+//
+//          int sum = number1+number2;
+//
+//        System.out.println("sum is :" +sum);
+
+
+
+
+
+
+    }
+}
